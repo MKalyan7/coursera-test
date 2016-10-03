3913f85d3845c7b6d50f75395636fc4fe13497e5
Kalyan Manam <kalyanmanam@Kalyans-Mini.home>
2016-10-03 00:18:52 -0700

    using the injection

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,25 +1,32 @@
-(function functionName() {
+(function () {
   'use strict';
   angular.module('LunchCheck',[])
-  .controller('LunchCheckController',function($scope){
-     $scope.name="";
-     $scope.diplayMessage = "";
-     $scope.displayMeals = function (){
-        var displyValue =calculateNumericForString($scope.name)
-        $scope.diplayMessage = displyValue;
-     };
+  .controller('LunchCheckController',LunchCheckController);
 
-     function calculateNumericForString(string){
-       var  mealsList = string.split(",");
-       var displyValue ="";
-       if(mealsList.length > 3){
-          displyValue = "Too much!";
-       } else {
-          displyValue = "Enjoy!";
-       }
-       return displyValue;
-     }
+  function LunchCheckController ($scope){
+    $scope.name="";
+    $scope.diplayMessage = "";
+    $scope.display = function(){
+       $scope.diplayMessage = displayMeals($scope.name);
+    };
 
-  });
+    function displayMeals(string){
+      console.log(string);
+      var  mealsList = string.split(",");
+      var displyValue ="";
+      if(mealsList.length > 3){
+         displyValue = "Too much!";
+      } else {
+         if(mealsList.length == 1 && mealsList[0] ==""){
+            displyValue = "Please enter data first!";
+         } else {
+           displyValue = "Enjoy!";
+         }
+      }
+      return displyValue;
+    }
+
+
+  }
 
 })();
